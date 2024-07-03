b21c62c046d932a4d9e83dd6d389b3f9ebab8c55
772vjrvj <42423955+772vjrvj@users.noreply.github.com>
2024-07-04 03:18:20 +0900

    ㄹㄹㄹs

--- main.py
+++ main.py
@@ -1,36 +1,21 @@
-import time
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
+import json
+import time
+import os
 import pandas as pd
-
-
-class Product:
-    def __init__(self, image_url, name, price, category):
-        self.image_url = image_url
-        self.name = name
-        self.price = price
-        self.category = category
-
-    def to_dict(self):
-        return {
-            "이미지URL": self.image_url,
-            "품명": self.name,
-            "가격": self.price,
-            "카테고리": self.category
-        }
 
 def setup_driver():
     chrome_options = Options()
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--incognito")
     chrome_options.add_argument("--window-size=1080,750")
-    chrome_options.add_argument("--remote-debugging-port=9222")
     user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
     chrome_options.add_argument(f'user-agent={user_agent}')
     chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
@@ -46,78 +31,69 @@ def setup_driver():
     })
     return driver
 
-def extract_product_info(driver, url):
+def fetch_product_info(driver, url):
     driver.get(url)
-    time.sleep(3)
+    time.sleep(30)  # 페이지가 완전히 로드되도록 대기
 
-    products = []
+    product_info = {}
 
-    # _1kp0Y6pSXs 클래스를 포함하는 요소를 찾기
     try:
-        container = driver.find_element(By.CLASS_NAME, "_1kp0Y6pSXs")
-        product_elements = container.find_elements(By.CLASS_NAME, "_3vb8-MtIrC")
-        for product_element in product_elements:
-            try:
-                image_url = product_element.find_element(By.TAG_NAME, "img").get_attribute("src")
-                name = product_element.find_element(By.CLASS_NAME, "_3tUCsjeUwv").text
-                price = product_element.find_element(By.CLASS_NAME, "_2OV7zzQ3Rj").text
-                category = "다른 구성"
-                product = Product(image_url, name, price, category)
-                products.append(product)
-            except Exception as e:
-                print(f"Error extracting product info: {e}")
+        product_info['상품명'] = driver.find_element(By.CLASS_NAME, "WBVL_7").text
     except Exception as e:
-        print(f"Error finding _1kp0Y6pSXs container: {e}")
+        product_info['상품명'] = None
+        print(f"Error fetching 상품명: {e}")
 
-    # OdAoJ-HKEa 클래스를 포함하는 요소를 찾기
     try:
-        containers_best_together = driver.find_elements(By.CLASS_NAME, "OdAoJ-HKEa")
-
-        # 첫 번째 OdAoJ-HKEa 처리 (베스트 상품)
-        best_product_elements = containers_best_together[0].find_elements(By.CLASS_NAME, "_1rY1-Sog8x")
-        for product_element in best_product_elements:
-            try:
-                image_url = product_element.find_element(By.CLASS_NAME, "_25CKxIKjAk").get_attribute("src")
-                name = product_element.find_element(By.CLASS_NAME, "_33pMQzgHDp").text
-                price = product_element.find_element(By.CLASS_NAME, "_2CREpuMwk6").text
-                category = "베스트 상품"
-                product = Product(image_url, name, price, category)
-                products.append(product)
-            except Exception as e:
-                print(f"Error extracting best product info: {e}")
-
-        # 두 번째 OdAoJ-HKEa 처리 (함께 구매)
-        together_product_elements = containers_best_together[1].find_elements(By.CLASS_NAME, "_1rY1-Sog8x")
-        for product_element in together_product_elements:
-            try:
-                image_url = product_element.find_element(By.CLASS_NAME, "_25CKxIKjAk").get_attribute("src")
-                name = product_element.find_element(By.CLASS_NAME, "_33pMQzgHDp").text
-                price = product_element.find_element(By.CLASS_NAME, "_2CREpuMwk6").text
-                category = "함께 구매"
-                product = Product(image_url, name, price, category)
-                products.append(product)
-            except Exception as e:
-                print(f"Error extracting together product info: {e}")
+        product_info['댓글'] = driver.find_element(By.CLASS_NAME, "F9RHbS").text
     except Exception as e:
-        print(f"Error finding OdAoJ-HKEa containers: {e}")
+        product_info['댓글'] = None
+        print(f"Error fetching 댓글: {e}")
 
-    return products
+    try:
+        product_info['판매'] = driver.find_element(By.CLASS_NAME, "AcmPRb").text
+    except Exception as e:
+        product_info['판매'] = None
+        print(f"Error fetching 판매: {e}")
 
-def fetch_excel(products, filename):
-    products_dict_list = [product.to_dict() for product in products]
-    df = pd.DataFrame(products_dict_list)
-    df.to_excel(filename, index=False)
-    print(f"Products have been successfully saved to '{filename}'")
+    try:
+        product_info['대표이미지'] = driver.find_element(By.CSS_SELECTOR, "img.IMAW1w").get_attribute("src")
+    except Exception as e:
+        product_info['대표이미지'] = None
+        print(f"Error fetching 대표이미지: {e}")
 
+    try:
+        product_info['설명'] = driver.find_element(By.CLASS_NAME, "QN2lPu").text
+    except Exception as e:
+        product_info['설명'] = None
+        print(f"Error fetching 설명: {e}")
+
+    return product_info
 
 def main():
-    url = "https://smartstore.naver.com/from_eat/products/5233032940"
+    urls = [
+        "https://shopee.co.id/thermostat-termostat-Kia-Rio-SF-Carrens-88%C2%B0C-0K2C0-15-171-Ori-i.1009594725.18594798407",
+        "https://shopee.co.id/thermostat-thermostart-termostat-kia-sportage-2-82%C2%B0C-25500-23010-i.1009594725.23859582857",
+        "https://shopee.co.id/fuel-tank-unit-pelampung-bensin-minyak-kia-pride-Q0K24-060-960A-i.1009594725.21592567358",
+        "https://shopee.co.id/actuator-idle-speed-servo-only-getz-verna-matrix-avage-35150-22600-i.1009594725.22447848339",
+        "https://shopee.co.id/thermostat-termostat-atoz-1.1cc-visto-1.1cc-82%C2%B0C-25500-02550-i.1009594725.22185071297",
+        "https://shopee.co.id/thermostat-termostat-picanto-atoz-1.0cc-visto-1.0cc-82%C2%B0C-25500-02500-i.1009594725.21494781438",
+        "https://shopee.co.id/Tie-rod-tierod-kanan-kiri-1set-kia-all-new-rio-grand-avega-original-i.1009594725.21286663932"
+    ]
+
     driver = setup_driver()
-    products = extract_product_info(driver, url)
+
+    product_info_list = []
+
+    for url in urls:
+        product_info = fetch_product_info(driver, url)
+        product_info_list.append(product_info)
+
     driver.quit()
 
     # 엑셀 파일로 저장
-    fetch_excel(products, "products_info.xlsx")
+    df = pd.DataFrame(product_info_list)
+    df.to_excel("product_info.xlsx", index=False)
+    print("엑셀 파일 저장 완료: product_info.xlsx")
 
 if __name__ == "__main__":
     main()
