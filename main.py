7221558888cde8517e0be37ae816de78e8430f72
772vjrvj <42423955+772vjrvj@users.noreply.github.com>
2024-10-04 00:54:24 +0900

    ㄹㄹㄹs

--- main.py
+++ main.py
@@ -1,145 +1,156 @@
-from appium import webdriver
-from appium.webdriver.common.appiumby import AppiumBy
-from appium.options.android import UiAutomator2Options
-from selenium.webdriver.common.action_chains import ActionChains
+import json
+import pandas as pd
+import math
+import requests
+import time
+import random
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import StaleElementReferenceException
-import pandas as pd
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import NoSuchElementException, TimeoutException, ElementNotInteractableException, WebDriverException
+from selenium.webdriver.common.action_chains import ActionChains
 
-import time
-
-def scroll_down(driver):
-    """W3C Actions API를 사용하여 스와이프 제스처를 구현"""
-    screen_size = driver.get_window_size()
-    start_x = screen_size['width'] // 2
-    start_y = screen_size['height'] * 5 // 6  # 화면 아래쪽 5/6 지점에서 시작
-    end_y = screen_size['height'] // 6  # 화면 위쪽 1/6 지점에서 끝
-
-    actions = ActionChains(driver)
-    actions.w3c_actions.pointer_action.move_to_location(start_x, start_y)
-    actions.w3c_actions.pointer_action.pointer_down()
-    actions.w3c_actions.pointer_action.move_to_location(start_x, end_y)
-    actions.w3c_actions.pointer_action.release()
-    actions.perform()
-
-    time.sleep(2)  # 스크롤 후 대기
-
-def scroll_and_collect_buttons(driver, max_buttons=150):
-    collected_buttons = set()
-    scroll_attempts = 0
-
-    while len(collected_buttons) < max_buttons:  # 최대 스크롤 시도 횟수 제한
-        try:
-            buttons = driver.find_elements(AppiumBy.ID, "com.instagram.android:id/image_button")
-            for button in buttons:
-                content_desc = button.get_attribute("content-desc")
-                if content_desc not in collected_buttons:
-                    collected_buttons.add(content_desc)
-
-            print(f"현재 수집된 버튼 개수: {len(collected_buttons)}")
-            scroll_down(driver)
-            scroll_attempts += 1
-
-        except StaleElementReferenceException:
-            print("StaleElementReferenceException 발생, 요소를 다시 찾습니다.")
-            scroll_down(driver)  # 스크롤 후 재탐색
-            time.sleep(2)
-
-        except Exception as e:
-            print(f"스크롤 중 다른 오류 발생: {e}")
-            break
-
-    return list(collected_buttons)[:max_buttons]
-
-def extract_usernames(collected_buttons):
-    """'님의 사진' 앞에 있는 사용자 이름만 추출하는 함수"""
-    usernames = []
-    for content_desc in collected_buttons:
-        if '님의 사진' in content_desc:
-            username = content_desc.split('님의 사진')[0]
-            usernames.append(username)
-    return usernames
-
-
-def save_usernames_to_excel(user_data, file_name="usernames.xlsx"):
-    """사용자 이름과 해시태그 리스트를 엑셀 파일로 저장하거나 업데이트하는 함수"""
+# 드라이버 세팅
+def setup_driver():
     try:
-        # 기존 엑셀 파일을 읽어온다 (파일이 있을 경우)
-        existing_data = pd.read_excel(file_name)
-        df_existing = pd.DataFrame(existing_data)
-    except FileNotFoundError:
-        # 파일이 없을 경우 새로 만든다
-        df_existing = pd.DataFrame(columns=["Usernames", "Hashtag"])
+        chrome_options = Options()
+        # chrome_options.add_argument("--headless")  # Run in headless mode if necessary
+        chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--incognito")  # Use incognito mode
 
-    # 새로운 데이터를 추가한다
-    df_new = pd.DataFrame(user_data, columns=["Usernames", "Hashtag"])
-    df_combined = pd.concat([df_existing, df_new], ignore_index=True).drop_duplicates()
+        user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+        chrome_options.add_argument(f'user-agent={user_agent}')
+        chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        chrome_options.add_experimental_option('useAutomationExtension', False)
 
-    # 엑셀 파일에 저장
-    df_combined.to_excel(file_name, index=False)
-    print(f"엑셀 파일 '{file_name}'에 사용자 이름과 해시태그가 업데이트되었습니다.")
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+        driver.execute_cdp_cmd('Page.addScriptToEvaluateOnNewDocument', {
+            'source': '''
+                Object.defineProperty(navigator, 'webdriver', {
+                  get: () => undefined
+                })
+            '''
+        })
 
+        return driver
+    except WebDriverException as e:
+        print(f"Error setting up the WebDriver: {e}")
+        return None
 
 def main():
-    options = UiAutomator2Options()
-    options.platform_name = "Android"
-    options.platform_version = "9"
-    options.device_name = "emulator-5554"
-    options.app_package = "com.instagram.android"
-    options.app_activity = "com.instagram.mainactivity.InstagramMainActivity"
-    options.no_reset = True
-    options.adb_exec_timeout = 20000
-    options.adb_executable = "C:\\Users\\772vj\\AppData\\Local\\Android\\Sdk\\platform-tools\\adb.exe"
+    driver = setup_driver()
 
-    driver = webdriver.Remote(command_executor='http://localhost:4723/wd/hub', options=options)
+    if not driver:
+        return
 
-    hashtags = [
-        "#kbeautyreview", "#kbeautyroutine", "#sensitiveskin",
-        "#fordryskin", "#veganskincareproducts", "#acneroutine"
-    ]
-    user_data = set()  # 사용자 이름과 해시태그를 저장할 세트 (중복 제거를 위해)
+    driver.get("https://www.instagram.com/")
+    id = input("로그인이 완료되면 아이디를 입력하세요 : ")
 
     try:
-        wait = WebDriverWait(driver, 30)
+        url = f"https://www.instagram.com/{id}"
+        driver.get(url)
+        time.sleep(5)
 
-        for hashtag in hashtags:
-            search_icon = wait.until(EC.element_to_be_clickable((AppiumBy.XPATH, '//android.widget.FrameLayout[@content-desc="검색 및 탐색하기"]/android.widget.ImageView')))
-            search_icon.click()
-            time.sleep(3)
+        # PolarisNavigationIcons 클래스명을 가진 두 번째 div 안의 a 태그 클릭
+        try:
+            # XPath를 contains로 수정하여 각 클래스를 포함한 div 요소 찾기
+            navigation_icons = WebDriverWait(driver, 10).until(
+                EC.presence_of_all_elements_located(
+                    (By.XPATH, "//*[contains(@class, 'x1iyjqo2') and contains(@class, 'xh8yej3')]")
+                )
+            )
+            # navigation_icons 안의 div 요소들 찾기
+            second_div = navigation_icons[0].find_element(By.XPATH, "./div[2]")
+            pressable_link = second_div.find_element(By.TAG_NAME, "a")
+            pressable_link.click()
 
-            # 검색 창에 포커스를 주기 위해 검색 상자를 클릭
-            search_box = wait.until(EC.presence_of_element_located((AppiumBy.ID, "com.instagram.android:id/action_bar_search_edit_text")))
-            search_box.click()
+            # 검색 input 필드에 'SKINSIDER' 입력
+            search_input = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, "//input[@aria-label='입력 검색']"))
+            )
+            search_input.clear()  # 기존 입력 값이 있을 경우 삭제
+            search_input.send_keys("SKINSIDER")
+
+            # 클래스 이름을 가진 div 내부에서 a 태그들을 찾음
             time.sleep(2)
+            div_element = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located(
+                    (By.XPATH, "//*[contains(@class, 'x9f619') and contains(@class, 'x78zum5') and contains(@class, 'xdt5ytf') and contains(@class, 'x1iyjqo2') and contains(@class, 'x6ikm8r') and contains(@class, 'x1odjw0f') and contains(@class, 'xh8yej3') and contains(@class, 'xocp1fn')]")
+                )
+            )
 
-            # 텍스트 입력 및 검색 실행
-            search_box.clear()
-            search_box.send_keys(hashtag)
-            driver.tap([(search_box.location['x'] + 10, search_box.location['y'] + 10)])  # 포커스 맞춤
-            time.sleep(1)
-
-            # 검색 결과 클릭
-            search_result = wait.until(EC.element_to_be_clickable((AppiumBy.ID, "com.instagram.android:id/row_hashtag_container")))
-            search_result.click()
+            # div 안에 있는 모든 a 태그의 href 속성 추출
+            # a 태그들이 로드될 때까지 기다림
             time.sleep(2)
+            a_tags = WebDriverWait(div_element, 10).until(
+                EC.presence_of_all_elements_located((By.TAG_NAME, "a"))
+            )
+            hrefs = [a.get_attribute("href") for a in a_tags]
+            # href 배열 출력
+            print(hrefs)
 
-            # 스크롤하면서 버튼 수집
-            collected_buttons = scroll_and_collect_buttons(driver, max_buttons=150)
+            # href 배열의 각 값으로 새로운 페이지를 열기
+            for href in hrefs:
+                driver.get(href)
+                print(f"Opened {href}")
 
-            # 사용자 이름 추출 및 중복 제거
-            usernames = extract_usernames(collected_buttons)
-            for username in usernames:
-                user_data.add((username, hashtag))
+                time.sleep(3)  # 페이지가 로드될 시간을 주기 (필요에 따라 조정 가능)
 
-            # 사용자 이름과 해시태그를 엑셀에 저장 (한 번의 해시태그 작업이 끝날 때마다)
-            save_usernames_to_excel(list(user_data), "usernames.xlsx")
+                # user 텍스트 추출
+                # user 텍스트 추출 (여러 클래스 이름을 포함하는 요소)
+                user = WebDriverWait(driver, 10).until(
+                    EC.presence_of_element_located(
+                        (By.XPATH, "//*[contains(@class, 'x1lliihq') and contains(@class, 'x193iq5w') and contains(@class, 'x6ikm8r') and contains(@class, 'x10wlt62') and contains(@class, 'xlyipyv') and contains(@class, 'xuxw1ft')]")
+                    )
+                ).text
 
+                # 두 번째 follower 텍스트 추출
+                follower = WebDriverWait(driver, 10).until(
+                    EC.presence_of_all_elements_located(
+                        (By.XPATH, "//*[contains(@class, 'xdj266r') and contains(@class, 'x11i5rnm') and contains(@class, 'xat24cr') and contains(@class, 'x1mh8g0r') and contains(@class, 'xexx8yu') and contains(@class, 'x4uap5') and contains(@class, 'x18d9i69') and contains(@class, 'xkhd6sd') and contains(@class, 'x1hl2dhg') and contains(@class, 'x16tdsg8') and contains(@class, 'x1vvkbs')]")
+                    )
+                )[1].text  # 두 번째 요소 선택
+
+                # content 텍스트 추출
+                content = WebDriverWait(driver, 10).until(
+                    EC.presence_of_element_located(
+                        (By.XPATH, "//*[contains(@class, '_ap3a') and contains(@class, '_aaco') and contains(@class, '_aacu') and contains(@class, '_aacx') and contains(@class, '_aad7') and contains(@class, '_aade')]")
+                    )
+                ).text
+
+                # url 텍스트 추출
+                url = WebDriverWait(driver, 10).until(
+                    EC.presence_of_element_located(
+                        (By.XPATH, "//*[contains(@class, 'x1lliihq') and contains(@class, 'x1plvlek') and contains(@class, 'xryxfnj') and contains(@class, 'x1n2onr6') and contains(@class, 'x1ff1cvt') and contains(@class, 'xatrb82') and contains(@class, 'x193iq5w') and contains(@class, 'xeuugli') and contains(@class, 'x1fj9vlw') and contains(@class, 'x13faqbe') and contains(@class, 'x1vvkbs') and contains(@class, 'x1s928wv') and contains(@class, 'xhkezso') and contains(@class, 'x1gmr53x') and contains(@class, 'x1cpjm7i') and contains(@class, 'x1fgarty') and contains(@class, 'x1943h6x') and contains(@class, 'x1i0vuye') and contains(@class, 'xvs91rp') and contains(@class, 'x1s688f') and contains(@class, 'x7l2uk3') and contains(@class, 'x10wh9bi') and contains(@class, 'x1wdrske') and contains(@class, 'x8viiok') and contains(@class, 'x18hxmgj')]")
+                    )
+                ).text
+                # 데이터 객체 생성
+                data = {
+                    "user": user,
+                    "follower": follower,
+                    "content": content,
+                    "url": url
+                }
+
+                # 객체 출력
+                print(data)
+
+
+        except (NoSuchElementException, TimeoutException) as e:
+            print(f"Error finding the second navigation icon or 'a' tag: {e}")
 
     except Exception as e:
-        print(f"오류 발생: {e}")
-    finally:
-        driver.quit()
+        print("Element not found", e)
 
+    time.sleep(2)
+    driver.quit()
+
+# 인스타
 if __name__ == "__main__":
     main()
