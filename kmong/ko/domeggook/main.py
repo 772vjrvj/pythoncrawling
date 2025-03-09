dd1d4e3a4e4ff762c02da14d15308519516670ba
772vj <772vjrvj@gmail.com>
2025-03-10 01:06:28 +0900

    크림 최종

--- kmong/ko/domeggook/main.py
+++ kmong/ko/domeggook/main.py
@@ -1,14 +1,200 @@
-from PyQt5.QtWidgets import QApplication
-from src.app_manager import AppManager
-from src.utils.singleton import GlobalState
+import time
 
-import sys
+import requests
+from bs4 import BeautifulSoup
+import re
+import random
 
-# 메인함수
+
+def fetch_item_ids(sw, pg):
+    url = f"https://domeggook.com/main/item/itemList.php?sfc=id&sf=id&sw={sw}&sz=100&pg={pg}"
+
+    headers = {
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        "accept-encoding": "gzip, deflate, br, zstd",
+        "accept-language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
+        "connection": "keep-alive",
+        "host": "domeggook.com",
+        "referer": f"https://domeggook.com/main/item/itemList.php?sfc=id&sf=id&sw={sw}&sz=100&pg={pg-1}",
+        "sec-ch-ua": '"Not(A:Brand";v="99", "Google Chrome";v="133", "Chromium";v="133"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"Windows"',
+        "sec-fetch-dest": "document",
+        "sec-fetch-mode": "navigate",
+        "sec-fetch-site": "same-origin",
+        "sec-fetch-user": "?1",
+        "upgrade-insecure-requests": "1",
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36"
+    }
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code != 200:
+        print("페이지를 불러오지 못했습니다.")
+        return []
+
+    soup = BeautifulSoup(response.text, "html.parser")
+    item_list = []
+
+    # ol 태그 내의 li 요소 찾기
+    ol_tags = soup.find_all("ol", class_="lItemList")
+    if ol_tags:
+        last_ol_tag = ol_tags[-1]  # 마지막 ol 태그 선택
+        li_tags = last_ol_tag.find_all("li")
+
+        for li in li_tags:
+            # li 내부의 a 태그 class="thumb" 찾기
+            a_tag = li.find("a", class_="thumb")
+            if a_tag and "href" in a_tag.attrs:
+                href = a_tag["href"]
+
+                # 정규식을 사용하여 숫자만 추출
+                match = re.search(r"/(\d+)", href)
+                if match:
+                    item_list.append(match.group(1))
+
+    return item_list
+
+
+def fetch_item_cnt(sw):
+    url = f"https://domeggook.com/main/item/itemList.php?sfc=id&sf=id&sw={sw}&sz=100&pg=1"
+
+    headers = {
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        "accept-encoding": "gzip, deflate, br, zstd",
+        "accept-language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
+        "connection": "keep-alive",
+        "host": "domeggook.com",
+        "referer": f"https://domeggook.com/main/item/itemList.php?sfc=id&sf=id&sw={sw}&sz=100&pg=0",
+        "sec-ch-ua": '"Not(A:Brand";v="99", "Google Chrome";v="133", "Chromium";v="133"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"Windows"',
+        "sec-fetch-dest": "document",
+        "sec-fetch-mode": "navigate",
+        "sec-fetch-site": "same-origin",
+        "sec-fetch-user": "?1",
+        "upgrade-insecure-requests": "1",
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36"
+    }
+
+    response = requests.get(url, headers=headers)
+    total_cnt = 0
+    total_page = 0
+    if response.status_code != 200:
+        print("페이지를 불러오지 못했습니다.")
+        return total_cnt, total_page
+
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    # div id="lCnt" 내부에서 숫자 찾기
+    lcnt_div = soup.find("div", id="lCnt")
+
+    if lcnt_div:
+        b_tag = lcnt_div.find("b")  # <b> 태그 직접 찾기
+        if b_tag:
+            total_cnt = int(b_tag.text.replace(",", ""))  # 콤마 제거 후 정수 변환
+            total_page = (total_cnt // 100) + (1 if total_cnt % 100 > 0 else 0)  # 페이지 계산
+
+
+    return total_cnt, total_page
+
+
+
+def fetch_product_details(product_id):
+    base_url = "https://domeggook.com/"
+    headers = {
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        "accept-encoding": "gzip, deflate, br, zstd",
+        "accept-language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
+        "connection": "keep-alive",
+        "host": "domeggook.com",
+        "sec-ch-ua": '"Not(A:Brand";v="99", "Google Chrome";v="133", "Chromium";v="133"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"Windows"',
+        "sec-fetch-dest": "document",
+        "sec-fetch-mode": "navigate",
+        "sec-fetch-site": "none",
+        "sec-fetch-user": "?1",
+        "upgrade-insecure-requests": "1",
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36"
+    }
+
+    url = f"{base_url}{product_id}"
+    response = requests.get(url, headers=headers)
+
+    if response.status_code != 200:
+        print(f"❌ {product_id} 페이지를 불러오지 못했습니다.")
+
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    product_data = {
+        'URL': url
+    }
+
+    # ✅ 판매자명 추출
+    seller_tag = soup.find("button", id="lBtnShowSellerInfo")
+    if seller_tag:
+        seller_name = seller_tag.find("b").text.strip()
+        product_data["판매자명"] = seller_name
+    else:
+        product_data["판매자명"] = ""
+
+    # ✅ 상품번호 추출
+    product_num_tag = soup.find("div", id="lInfoHeader")
+    if product_num_tag:
+        match = re.search(r"상품번호\s*:\s*(\d+)", product_num_tag.text)
+        if match:
+            product_data["상품번호"] = match.group(1)
+        else:
+            product_data["상품번호"] = ""
+    else:
+        product_data["상품번호"] = ""
+
+    # ✅ 상품명 추출
+    product_name_tag = soup.find("h1", id="lInfoItemTitle")
+    product_data["상품명"] = product_name_tag.text.strip() if product_name_tag else ""
+
+    # ✅ 재고수량 추출
+    stock_tag = soup.find("td", class_="lInfoItemContent")
+    if stock_tag:
+        match = re.search(r"([\d,]+)", stock_tag.text)
+        if match:
+            product_data["재고수량"] = int(match.group(1).replace(",", ""))
+        else:
+            product_data["재고수량"] = 0
+    else:
+        product_data["재고수량"] = 0
+
+    return product_data
+
+
+
+
+
+
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    state = GlobalState()
-    state.initialize()
-    app_manager = AppManager()
-    app_manager.go_to_login()
-    sys.exit(app.exec_())
+
+    total_cnt, total_page = fetch_item_cnt("huigone7589")
+    print(f'total_cnt : {total_cnt}')
+    print(f'total_page : {total_page}')
+    all_item_list = []
+    # 테스트 실행
+    for i in range(1, total_page + 1):
+        print(f'index {i}')
+        item_list = fetch_item_ids("huigone7589", i)
+        print(f'item_list : {item_list}')
+        all_item_list.extend(item_list)
+        print(f'all len : {len(all_item_list)}')
+        time.sleep(random.uniform(2, 3))
+
+    print(f'all list : {all_item_list}')
+    result_list = []
+
+    for product_id in all_item_list:
+        obj = fetch_product_details(product_id)
+        print(f'obj : {obj}')
+        result_list.append(obj)
+        print(f'obj list : {result_list}')
+        time.sleep(random.uniform(2, 3))
+
+    print(f'{result_list}')
--- kmong/ko/domeggook/test/main_list.py
+++ kmong/ko/domeggook/test/main_list.py
@@ -1,67 +1,55 @@
 import requests
-import time
+from bs4 import BeautifulSoup
 import re
 
-# 요청 헤더 설정
-headers = {
-    "authority": "api.kream.co.kr",
-    "method": "GET",
-    "scheme": "https",
-    "accept": "application/json, text/plain, */*",
-    "accept-encoding": "gzip, deflate, br, zstd",
-    "accept-language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
-    "origin": "https://kream.co.kr",
-    "referer": "https://kream.co.kr/my/selling?tab=finished",
-    "sec-ch-ua": '"Not(A:Brand";v="99", "Google Chrome";v="133", "Chromium";v="133"',
-    "sec-ch-ua-mobile": "?0",
-    "authorization": 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6dHJ1ZSwiaWF0IjoxNzQwNDA3MTYyLCJqdGkiOiJkYThlNWU3ZS1lOTJjLTQ4ZDQtODY3My1mODUwOGU2ZGU3MzAiLCJ0eXBlIjoiYWNjZXNzIiwiaWRlbnRpdHkiOjYyNDA2NzUsIm5iZiI6MTc0MDQwNzE2MiwiY3NyZiI6ImQ1Y2Q0YjNjLWNkZTItNDFkNy1hNDNlLWI3NjFjYjBmZDdjZCIsImV4cCI6MTc0MDQxNDM2MiwidWMiOnsic2FmZSI6dHJ1ZX0sInVkIjoiLmVKeEZqa3RMdzBBVWhmX0taVll0ZENienpNMWpKUzRVYXQwWTZESk1NcmM2TkcxQ0dxMFBfTzhtS01qWmZlY2N6dmxpWVdJRjAxSTdMalhYdGxLMjBHa2h0VURVdWN2WWhnVjZpeTNWMDhkQWNfUkt6VC1MNFplVWlHZ2IxMXFPVGhLM2hNaWJfT0I1RnRBb2JVMnVVTTYxa1U3OVJMVVBZVnhXVlNhVVJhR01GdHFrc185Nm9iSDJ6M1JlVHUzNno5aDFQbkZDd21vZno2R19YdUN4QWlXRkxHRUdxUzNoUGJWcnVCbUdqdmJVYk9PVU9JUENwTERhM2xlN2h3MTA4VWh3Ui0yeFg4UHR5OWlmS0ZIR0NMa0ludnpCal9HdndyNV9BTmFqU3BnLlNtMjFfNEl0am5Jc0kwNDBEaEVMMnhBb1lQayJ9.MGHRDNsh0b-U7_Lh23tnYPUWT8TzuvITBKOf-Sljp7c',
-    "sec-ch-ua-platform": '"Windows"',
-    "sec-fetch-dest": "empty",
-    "sec-fetch-mode": "cors",
-    "sec-fetch-site": "same-site",
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36",
-    "x-kream-api-version": "41",
-    "x-kream-client-datetime": "20250223183955+0900",
-    "x-kream-device-id": "web;fcb9350d-91d8-4a78-945d-e941984c6386",
-    "x-kream-web-build-version": "6.7.4",
-    "x-kream-web-request-secret": "kream-djscjsghdkd"
-}
+def fetch_item_ids(sw, pg):
+    url = f"https://domeggook.com/main/item/itemList.php?sfc=id&sf=id&sw={sw}&sz=100&pg={pg}"
 
-# URL 패턴 정규표현식 (숫자만 추출)
-url_pattern = re.compile(r"https://kream\.co\.kr/my/selling/(\d+)")
-extracted_ids = []
+    headers = {
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        "accept-encoding": "gzip, deflate, br, zstd",
+        "accept-language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
+        "connection": "keep-alive",
+        "host": "domeggook.com",
+        "referer": f"https://domeggook.com/main/item/itemList.php?sfc=id&sf=id&sw={sw}&sz=100&pg={pg-1}",
+        "sec-ch-ua": '"Not(A:Brand";v="99", "Google Chrome";v="133", "Chromium";v="133"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"Windows"',
+        "sec-fetch-dest": "document",
+        "sec-fetch-mode": "navigate",
+        "sec-fetch-site": "same-origin",
+        "sec-fetch-user": "?1",
+        "upgrade-insecure-requests": "1",
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36"
+    }
 
-def fetch_data(cursor):
-    # status=expired
-    # status=canceled
-    tab="finished"
-    status="canceled"
-    request_key="a513410c-91b9-484c-8275-97e99a9eb2d2"
-
-
-    url = f"https://api.kream.co.kr/api/o/asks/?cursor={cursor}&tab={tab}&status={status}&request_key={request_key}"
     response = requests.get(url, headers=headers)
 
-    if response.status_code == 200:
-        data = response.json()
-        if "items" in data:
-            for item in data["items"]:
-                if item.get("actions", []):
-                    for action in item.get("actions", []):
-                        if "value" in action and "https://kream.co.kr/my/selling/" in action["value"]:
-                            match = url_pattern.search(action["value"])
-                            if match:
-                                print(match.group(1))
-                                extracted_ids.append(match.group(1))  # 숫자 부분만 저장
-            print(f"Cursor {cursor}: {extracted_ids}")
-    else:
-        print(f"Cursor {cursor}: 요청 실패 (HTTP {response.status_code})")
+    if response.status_code != 200:
+        print("페이지를 불러오지 못했습니다.")
+        return []
 
-def main():
-    for cursor in range(1, 30):  # 1부터 52까지 반복
-        fetch_data(cursor)
-        time.sleep(1)  # 1초 대기
-    print(extracted_ids)
+    soup = BeautifulSoup(response.text, "html.parser")
+    item_list = []
 
-if __name__ == "__main__":
-    main()
+    # ol 태그 내의 li 요소 찾기
+    ol_tag = soup.find("ol", class_="lItemList")
+    if ol_tag:
+        li_tags = ol_tag.find_all("li")
+
+        for li in li_tags:
+            # li 내부의 a 태그 class="thumb" 찾기
+            a_tag = li.find("a", class_="thumb")
+            if a_tag and "href" in a_tag.attrs:
+                href = a_tag["href"]
+
+                # 정규식을 사용하여 숫자만 추출
+                match = re.search(r"/(\d+)", href)
+                if match:
+                    item_list.append(match.group(1))
+
+    print(item_list)
+    return item_list
+
+# 테스트 실행
+fetch_item_ids("huigone7589", 3)
